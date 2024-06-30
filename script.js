dffa6ef8e218016436fa2aed9d2bdca42d488b81
abhishek1959 <abhishekpraj2002@gmail.com>
2024-06-30 22:08:19 +0530

    header responsive

--- script.js
+++ script.js
@@ -1,33 +1,52 @@
+
+        // JavaScript to handle the active class
+        document.querySelectorAll('.nav__list a').forEach(item => {
+            item.addEventListener('click', event => {
+                // Remove the active class from all items
+                document.querySelectorAll('.nav__list a').forEach(link => {
+                    link.classList.remove('active-link');
+                });
+
+                // Add the active class to the clicked item
+                event.target.classList.add('active-link');
+            });
+        });
+
+
+
+
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
+/** const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
-
+**/
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
+/** if(navToggle){
     navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu')
     })
 }
+    **/
+
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
+/** if(navClose){
+ navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
     })
-}
+} **/
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+/** const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction)) **/
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader(){
@@ -48,7 +67,7 @@ window.addEventListener('scroll', scrollUp)
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
+/** function scrollActive(){
     const scrollY = window.pageYOffset
 
     sections.forEach(current =>{
@@ -63,7 +82,7 @@ function scrollActive(){
         }
     })
 }
-window.addEventListener('scroll', scrollActive)
+window.addEventListener('scroll', scrollActive) **/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
